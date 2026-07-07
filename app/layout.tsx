e42aac5c1f734c22a93fa313205b7b5c092c4d60
qnixs <artemevrov@gmail.com>
2026-07-07 17:43:38 +0500

    Вроде_метрику_подключил

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import { Analytics } from '@vercel/analytics/next'
 import type { Metadata, Viewport } from 'next'
-import { YandexMetrika } from '@/components/yandex-metrika'
+import Script from 'next/script'
 import './globals.css'
 
 export const metadata: Metadata = {
@@ -61,8 +61,47 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ru" className="bg-background">
+      <head>
+        {/* Yandex.Metrika counter */}
+        <Script
+          id="yandex-metrika"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(m,e,t,r,i,k,a){
+                m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                m[i].l=1*new Date();
+                for(var j=0;j<document.scripts.length;j++){if(document.scripts[j].src===r){return;}}
+                k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+              })(window,document,'script','https://mc.yandex.ru/metrika/tag.js?id=110481134','ym');
+              ym(110481134,'init',{
+                ssr:true,
+                webvisor:true,
+                clickmap:true,
+                ecommerce:"dataLayer",
+                referrer:document.referrer,
+                url:location.href,
+                accurateTrackBounce:true,
+                trackLinks:true
+              });
+            `,
+          }}
+        />
+        {/* /Yandex.Metrika counter */}
+      </head>
       <body className="antialiased font-sans">
-        <YandexMetrika />
+        <noscript>
+          <div>
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img
+              src="https://mc.yandex.ru/watch/110481134"
+              style={{ position: 'absolute', left: '-9999px' }}
+              alt=""
+              width="1"
+              height="1"
+            />
+          </div>
+        </noscript>
         {children}
         {process.env.NODE_ENV === 'production' && <Analytics />}
       </body>
